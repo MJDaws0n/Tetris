ad9827e18aaf63b059cf02ca2e6734dded1aba76
MJDawson <66313685+MJDaws0n@users.noreply.github.com>
2025-12-04 12:42:16 +0000

    Updated port bug

--- node-app/index.js
+++ node-app/index.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 const http = require('http');
 
 const app = express();
-const port = process.env.NODE_PORT || 3000;
+const port = 3000;
 
 // Database connection configuration
 const dbConfig = {
